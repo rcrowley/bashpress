216075101b0ad74ead7ec1b8b2f363e6728db4ed
Richard Crowley <r@rcrowley.org>
2008-08-25 22:27:32 -0700

    bin/drafts and some refactoring in bin/publish

---
+++ include/lib_dir.php
@@ -0,0 +1,29 @@
+<?php
+
+# Recursively walk the directory tree
+#   This special rscandir function will return a tree of only directories
+function dir_rscandir($dir, $all = true) {
+	$ls = array_diff(scandir($dir), array('.', '..'));
+	$out = array();
+	foreach ($ls as $l) {
+		if (is_dir("$dir/$l")) { $out[$l] = dir_rscandir("$dir/$l"); }
+		else if ($all) { $out[$l] = false; }
+	}
+	return sizeof($out) ? $out : false;
+}
+
+# Flatten the walked directory tree at every depth
+function dir_flatten($tree, $path = '', $depth = 1) {
+	$list = array();
+	foreach ($tree as $dir => $subtree) {
+		$list[$depth][] = "$path/$dir";
+		if ($subtree) {
+			$sublist = dir_flatten($subtree, "$path/$dir", $depth + 1);
+			foreach ($sublist as $d => $l) {
+				if (!is_array($list[$d])) { $list[$d] = array(); }
+				$list[$d] = array_merge($list[$d], $l);
+			}
+		}
+	}
+	return $list;
+}
